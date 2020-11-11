282da3f35bce1335d9565bf995449a2ca1f79ff5
Cullyn <cullyn.newman@gmail.com>
2020-11-10 20:24:25 -0800

    moved wallpapers, added config layout for workspace 5

--- rofi/notes.sh
+++ rofi/notes.sh
@@ -8,11 +8,12 @@
 c1="evolution"
 c2="organic-chemistry"
 c3="organismal-physiology"
+c4="config"
 
 # you can customise the rofi command all you want ...
 rofi_command="rofi -show drun -p Notes -theme notes.rasi"
 
-options="$c1\n$c2\n$c3"
+options="$c1\n$c2\n$c3\n$c4"
 
 # ... because the essential options (-dmenu and -p) are added here
 eval "~/scripts/$(echo -e "$options" | $rofi_command -dmenu -p "").sh"
