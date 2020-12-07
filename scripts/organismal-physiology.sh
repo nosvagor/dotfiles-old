75e1a914c5ac0082c3dc91fe90ac6fef4ee65653
Cullyn <cullyn.newman@gmail.com>
2020-12-06 19:06:34 -0800

    removing I3 layout scripts

--- scripts/organismal-physiology.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-i3-msg append_layout ~/.config/i3/org-phys.json &&
-code ~/Notes |
-alacritty |
-chromium |
-zathura ~/textbooks/Animal-Physiology-4th.pdf |
-zathura ~/Notes/organismal-physiology/org-phys.pdf |
-zathura ~/Notes/organismal-physiology/lecture-concepts.pdf |
