d84cb8900db9a88e8c24114b36cf8d6607a6a016
cullyn <cullyn.newman@gmail.com>
2020-12-14 01:48:55 -0800

    stupid hard coded settings, I know...

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -6,15 +6,13 @@
 
 poweroff_command="systemctl poweroff"
 reboot_command="systemctl reboot"
-logout_command="i3-msg exit"
+logout_command="bspc quit"
 hibernate_command="systemctl hibernate"
-suspend_command="systemctl suspend"
-lock_command="betterlockscreen -l dimblur"
 
 # you can customise the rofi command all you want ...
 rofi_command="rofi -no-lazy-grab -show drun -p Power -theme powermenu.rasi"
 
-options=$'poweroff\nreboot\nlock\nlogout\nhibernate\nsuspend' 
+options=$'poweroff\nreboot\nlogout\nhibernate' 
 
 # ... because the essential options (-dmenu and -p) are added here
 eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
