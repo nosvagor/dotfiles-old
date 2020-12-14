998cb81e2f233124611203573b98b636f6a3fea7
cullyn <cullyn.newman@gmail.com>
2020-12-14 00:09:20 -0800

    better polybar laucnh script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-pkill polybar
-polybar -r main &
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+
+polybar -r base &
+echo "Polybar launched..."
