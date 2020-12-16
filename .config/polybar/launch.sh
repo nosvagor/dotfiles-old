6213845328ca43ac94b18e1701455f80c24e90eb
cullyn <cullyn.newman@gmail.com>
2020-12-15 23:31:32 -0800

    polybar done for now, revisting layout script launcher later

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
-polybar -r base &
+polybar -r base & 
 echo "Polybar launched..."
--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -4,15 +4,16 @@
 # (last 3 dependencies are adjustable below)
 # tostiheld, 2016
 
-poweroff_command="systemctl poweroff"
-reboot_command="systemctl reboot"
-logout_command="bspc quit"
-hibernate_command="systemctl hibernate"
+1:  襤_command="systemctl poweroff"
+2:  _command="systemctl reboot"
+3:  _command="bspc quit"
+4:  ⏾_command="systemctl hibernate"
+5:  _command="lock"
 
 # you can customise the rofi command all you want ...
-rofi_command="rofi -no-lazy-grab -show drun -p Power -theme powermenu.rasi"
+rofi_command="rofi -no-lazy-grab -show drun -p 拉 -theme powermenu.rasi"
 
-options=$'poweroff\nreboot\nlogout\nhibernate' 
+options=$'1:  襤\n2:  \n3:  \n4:  ⏾\n5:  \n' 
 
 # ... because the essential options (-dmenu and -p) are added here
 eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
