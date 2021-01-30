fde568df1aedaba649b8bae8033e7785005dbe34
cullyn <cullyn.newman@gmail.com>
2021-01-30 15:45:27 -0800

    various updates

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -2,16 +2,13 @@
 
 bspc config focus_follows_pointer false
 
-bspc desktop -f ^2
-feh --bg-scale ~/resources/wallpapers/'jelly-3840x2160.jpg'
-
 code ~/notes/$1 &
 sleep 1.0
 
-bspc config split_ratio 0.38 && firefox -new-window https://github.com/cullyn-inverba/notes/tree/master/$1 & 
+bspc config split_ratio 0.37 && firefox -new-window https://github.com/cullyn-inverba/notes/tree/master/$1 & 
 sleep 1.0
 
-bspc config split_ratio 0.65 && kitty -e cava &
+bspc config split_ratio 0.67 && kitty -e cava &
 sleep 1.0
 
 bspc config split_ratio 0.13 && zathura ~/notes/$1/$1.pdf &
@@ -21,11 +18,6 @@ bspc node -f west.local
 bspc node -f west.local
 
 bspc config split_ratio 0.88 && kitty -d ~/notes &
-sleep 1.0 &&
-
-bspc node -f east.local 
-
-bspc config split_ratio 0.95 && zathura ~/textbooks/$1-text.pdf &
 sleep 1.0 &&
 
 bspc config split_ratio 0.5
