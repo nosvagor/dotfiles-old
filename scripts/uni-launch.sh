fb014e071030a656b9770488429964e1b3c8f720
cullyn <cullyn.newman@gmail.com>
2021-04-02 16:00:26 -0700

    various small updates

--- scripts/uni-launch.sh
+++ scripts/uni-launch.sh
@@ -15,6 +15,10 @@ bspc config split_ratio 0.13 && zathura ~/notes/$1/$1.pdf &
 sleep 1.0
 
 bspc node -f west.local
+
+bspc config split_ratio 0.95 && zathura ~/textbooks/$1-text.pdf &
+sleep 1
+
 bspc node -f west.local
 
 bspc config split_ratio 0.88 && kitty -d ~/notes &
