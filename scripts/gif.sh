75c1e0b7fb72a81d0a968b5985c2ac3856da5398
cullyn <cullyn.newman@gmail.com>
2021-04-29 09:21:50 -0700

    gifs much large mnay wow, mabye jpg will help. Not tested lol

--- scripts/gif.sh
+++ scripts/gif.sh
@@ -6,8 +6,8 @@ read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
 notify-send -u Critical -t 3000 started Recording
 
 ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE" 
-ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
-ffmpeg -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
+ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.jpg
+ffmpeg -i "$TMPFILE" -i /tmp/palette.jpg -filter_complex "paletteuse" $OUTPUT.gif
 mv $TMPFILE $OUTPUT.mkv
 
 notify-send -t 5000 "total size: $(du -h $OUTPUT.gif | awk '{print $1}')" "Gif Done"
