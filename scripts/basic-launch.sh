399150ea0ed658b708660481b40251bb3d6a654d
cullyn <cullyn.newman@gmail.com>
2021-08-06 11:38:24 -0700

    always start tiled, fixed vscode size

--- scripts/basic-launch.sh
+++ scripts/basic-launch.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 bspc config focus_follows_pointer false
+bspc desktop -l tiled
 
 feh --bg-scale ~/resources/wallpapers/'jelly-3840x2160.jpg'
 
 kitty -e cava &
-sleep 1
+sleep 0.69
 
 bspc config split_ratio 0.11 && bspc node -p south &&  code ~/$1 &
 sleep 1
 
 bspc config split_ratio 0.88 && bspc node -p south && kitty -e cava &
-sleep 1
+sleep 0.69
 
 bspc node -f north.local 
 
 bspc config split_ratio 0.42 && firefox --new-window https://github.com/cullyn-inverba/notes &
 sleep 1
 
-bspc config split_ratio 0.50
+bspc config split_ratio 0.48
 bspc config focus_follows_pointer true
 
 exit 0
--- scripts/code-launch.sh
+++ scripts/code-launch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-bspc config focus_follows_pointer false
+bspc config focus_follows_pointer false &
+bspc desktop -l tiled
 
 feh --bg-scale ~/resources/wallpapers/'jelly-3840x2160.jpg'
 
 kitty -e cava &
-sleep 1
+sleep 0.69
 
-bspc config split_ratio 0.33 && firefox -new-window https://calendar.google.com/calendar/u/0/r & 
+bspc config split_ratio 0.338 && firefox -new-window https://calendar.google.com/calendar/u/0/r & 
 sleep 1
 
 bspc node -f west.local 
